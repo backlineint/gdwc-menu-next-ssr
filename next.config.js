435e6e1c78dce3c0b4d72c1eb11db6a83c4eb158
Brian Perry <brian.perry@pantheon.io>
2023-06-22 16:58:37 -0500

    Server side rendered menu example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+// next.config.js
+const withLitSSR = require('@lit-labs/nextjs')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withLitSSR(nextConfig);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import '@gdwc/components/menu'
+import { denormalize } from "@drupal/decoupled-menu-parser";
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://dev-decoupled-drupal-contrib.pantheonsite.io/system/menu/main/linkset')
+  const menu = await res.json()
+  return { props: { menu } }
+}
+
+export default function Home({ menu }) {
+  const menuTree = denormalize(menu)[0].tree;
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <gdwc-menu
+        branding="Menu Heading"
+        theme="horizontal"
+        tree={JSON.stringify(menuTree)}>
+      </gdwc-menu>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
